f785d570a4a5aaee294f421ac72901898f7be3f6
htrengove <heath.trengove@plusit.com.au>
2016-10-21 09:57:39 +1100

    Powershell script for custom EXE/ADV sensor in PRTG
    This script can be used to monitor windows backups in PRTG

---
+++ wbackupcheck.ps1
@@ -0,0 +1,17 @@
+$DEVICE=$args[0]
+
+$BackupStatus = Invoke-Command -Computername $DEVICE -ScriptBlock { Get-WBSummary }
+
+"<prtg>"
+"<result>"   
+"<channel>"
+"BackupStatus"
+"</channel>"
+"<value>"
+echo $BackupStatus.LastBackupResultHR
+"</value>"
+"</result>"
+"<text>"
+echo "Last Successful Backup: $($BackupStatus.LastSuccessfulBackupTime)"
+"</text>"
+"</prtg>"
